a716f5abd38f64c47f372360077dc60fd217c3f5
Jose Arias <josee.ariasmelo@gmail.com>
2024-10-03 12:01:27 -0300

    refactor(db): update database configuration to use environment variables
    - Update the database configuration in `db.js` to use environment variables for the host, user, password, and database name.
    - Add the `dotenv` dependency to load the environment variables from a `.env` file.

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,30 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Cargar variables de entorno
 
 // Crear la conexión a la base de datos
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',     // Usuario predeterminado de XAMPP
-  password: '',     // Deja vacío si no configuraste una contraseña
-  database: 'estudiantes_db',
-  waitForConnections: true,  // Espera si no hay conexiones disponibles
-  connectionLimit: 10,       // Número máximo de conexiones en el pool
-  queueLimit: 0              // Número máximo de solicitudes en espera
+  host: process.env.DB_HOST, // Dirección del servidor MySQL
+  user: process.env.DB_USER, // Usuario predeterminado de XAMPP
+  password: process.env.DB_PASSWORD, // Contraseña de la base de datos
+  database: process.env.DB_NAME, // Nombre de la base de datos
+  waitForConnections: true, // Espera si no hay conexiones disponibles
+  connectionLimit: 10, // Número máximo de conexiones en el pool
+  queueLimit: 0 // Número máximo de solicitudes en espera
 });
 
 // Conectar a la base de datos
 pool.getConnection((err, connection) => {
   if (err) {
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_USER:', process.env.DB_USER);
+    console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+    console.log('DB_NAME:', process.env.DB_NAME);
     console.error('Error conectando a la base de datos:', err);
     return;
   }
+  
   console.log('Conexión exitosa a la base de datos MySQL.');
-  connection.release(); // Libera la conexión de vuelta al pool
+  connection.release(); // Libera la conexión de vuelta al pool  
 });
 
 module.exports = pool; // Exportar el pool
