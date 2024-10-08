d8d4af9a3ff6b7954c3f9e0e475099087e966698
Jose Arias <josee.ariasmelo@gmail.com>
2024-10-08 16:36:24 -0300

    refactor(routes): Update footballPlayerRoutes.js

--- src/routes/footballPlayerRoutes.js
+++ src/routes/footballPlayerRoutes.js
@@ -1,7 +1,7 @@
 // routes/footballPlayerRoutes.js
 const express = require('express');
 const FootballPlayerController = require('../controllers/footballPlayerController');
-const FootballPlayerApiController = require('../controllers/footballPlayarApiController');
+const FootballPlayerApiController = require('../controllers/FootballPlayerApiController');
 
 const router = express.Router();
 
@@ -9,14 +9,15 @@ const router = express.Router();
 router.post('/footballplayers', FootballPlayerController.create);
 router.get('/footballplayers', FootballPlayerController.getAll);
 router.get('/footballplayers/:id', FootballPlayerController.getById);
-router.put('/footballplayers/:id', FootballPlayerController.update);
+router.post('/footballplayers/update/:id', FootballPlayerController.update);
 router.delete('/footballplayers/:id', FootballPlayerController.delete);
+router.get('/footballplayers/edit/:id', FootballPlayerController.getEditPlayer);
 
 // Definir las rutas para CRUD de FootballPlayers API
 router.post('/api/footballplayers', FootballPlayerApiController.create);
 router.get('/api/footballplayers', FootballPlayerApiController.getAll);
 router.get('/api/footballplayers/:id', FootballPlayerApiController.getById);
-router.put('/api/footballplayers/:id', FootballPlayerApiController.update);
+router.put('/api/footballplayers/update/:id', FootballPlayerApiController.update);
 router.delete('/api/footballplayers/:id', FootballPlayerApiController.delete);
 
 module.exports = router;
